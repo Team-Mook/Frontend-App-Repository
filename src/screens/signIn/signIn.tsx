86ef2fee08cb7982ced8dc0c8cabc428aefdbd7a
JJHyeok13 <jjinh99@naver.com>
2024-11-04 04:39:43 +0900

    feat: 추가 설정 및 초기화면 구현

--- src/screens/signIn/signIn.tsx
+++ src/screens/signIn/signIn.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
+
+import AppleLogo from '@assets/signIn/appleLogo.svg';
+import GoogleLogo from '@assets/signIn/googleLogo.svg';
 
 const SignInScreen = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>SignInScreen</Text>
+    <SafeAreaView className="flex-1 bg-[#121213]">
+      <View className="flex flex-1 flex-col px-5 justify-end">
+        <View className="flex flex-col space-y-3 mb-10">
+          <Pressable className="flex flex-row items-center space-x-4 justify-center rounded-full bg-white p-3.5">
+            <GoogleLogo />
+            <Text className="text-base font-medium">Google로 로그인하기</Text>
+          </Pressable>
+          <Pressable className="flex flex-row items-center space-x-4 justify-center rounded-full bg-white p-3.5">
+            <AppleLogo />
+            <Text className="text-base font-medium">Apple로 로그인하기</Text>
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
