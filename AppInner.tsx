2ef5176823992d24c5d9f2ddad72b7e877f9706b
JJHyeok13 <jjinh99@naver.com>
2024-11-04 03:01:40 +0900

    feat: 기본 설정

---
+++ AppInner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+function AppInner() {
+  return <GestureHandlerRootView />;
+}
+
+export default AppInner;
